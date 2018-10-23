c0d160d736ab39d5261bb4d3ece51a6f30d71710
mjr390 <mjr117@yahoo.com>
2018-10-23 19:26:04 -0400

    added a new route for the output, currently showing test values. it returns a docstring to render the page

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # import dependencies
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 from flask_pymongo import PyMongo
 # import the function to return the ML'd data here
 
@@ -11,26 +11,29 @@ app = Flask(__name__)
 def njre():
     # create a link to whatever the zipcodes data source will be under here
     # Will most likely be AWS link
-    # app.config["MONGO_URI"] = 'mongodb://localhost:27017/NJre'
-    # mongo = PyMongo(app)
-    # db = mongo.db
+    app.config["MONGO_URI"] = 'mongodb://localhost:27017/NJRE_test'
+    mongo = PyMongo(app)
+    db = mongo.db
 
     # create a variable containing the data
-    # prefZips = db.NJre.find_one()
+    prefZips = db.flask_test.find_one()
 
     #testing json
-    prefZips = {
-        "zipcodes": ['07001', '07002', '07003'],
-        "Rating": [.62, .3, .08],
-        "Lat": [-72, -60, -65],
-        "Long": [120, 118, 110]
-    }
-    # print(prefZips)
-    # prefZips_1 = prefZips["<>"]
+    # prefZips = {
+    #     "zipcodes": ['07001', '07002', '07003'],
+    #     "Rating": [.62, .3, .08],
+    #     "Lat": [-72, -60, -65],
+    #     "Long": [120, 118, 110]
+    # }
+    print(prefZips)
+    prefZips_zip = prefZips["zipcodes"]
+    prefZips_rat = prefZips["rating"]
+    prefZips_lat = prefZips["Lat"]
+    prefZips_long = prefZips["Long"]
     
     # convert the pulled data into a json to render
-    # prefZips_dict  = {key:value}
-    return jsonify(prefZips)
+    prefZips_dict  = {"zipcode":prefZips_zip, 'rating':prefZips_rat, 'Lat': prefZips_lat, 'Long': prefZips_long}
+    return jsonify(prefZips_dict)
 
 
 # create a index route
@@ -38,6 +41,18 @@ def njre():
 def index():
     # Return the template 
     return render_template('index.html')
+
+@app.route('/output_page', methods=['POST'])
+def Out():
+    
+    parks = request.form['parks']
+    price = request.form['price']
+    tax = request.form['tax']
+    schools = request.form['schools']
+    crime = request.form['crime']
+    transportation = request.form['transportation']
+    eats = request.form['eats']
+    return """<h1>parks: %s price: %s tax: %s schools: %s crime: %s transport: %s eats: %s <br/> </h1> <a href="/">Back Home</a>""" % (parks, price, tax, schools, crime, transportation, eats)
 
 
 if __name__ == "__main__":
